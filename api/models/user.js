a1ad6c02e94dff8ed3bf4868d9215f053bb3e5c6
MUHAMMAD ASLAM A <aslamabdurrohim@gmail.com>
2017-05-17 14:01:42 +0700

    api baru

--- api/models/user.js
+++ api/models/user.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module.exports = function(sequelize, DataTypes) {
   return sequelize.define('user', {
     nama_user: DataTypes.STRING,
@@ -92,48 +91,3 @@ module.exports = function(sequelize, DataTypes) {
     }
   });
 };
-=======
-var conn = require('../connection.js');
-var crypto = require('crypto');
-var modtoken = require('../token.js');
-
-function User(){
-	this.session = function(req, res){
-		var data = req;
-		modtoken.checkToken(data, res);
-	}
-
-	this.login = function(data, res){
-		let email = data.email;
-		let pass = crypto.createHash('md5').update(data.pass).digest('hex');
-
-		if(email == '' || pass == ''){
-			res.json({"status": false, "message":"there is empty field"});
-		}else{
-			conn.acquire(function(err, con){
-				var query = 'SELECT * FROM ittoday WHERE email = ? and password = ?';
-				var query_data = [email, pass]; 
-				console.log(query_data);
-				con.query(query, query_data, function(err, result){
-					con.release();
-					if(err == null){
-						if(!result.length){
-							res.json({"status":false, "message":"Wrong email or password"});
-						}else{
-							var signInTime = Math.floor(Date.now()/1000); // iat
-							var expired = signInTime + (2*60*60) // exp after 2 hours
-							var data = {'id': result[0].id_user, 'nama': result[0].nama_lengkap, 'email': result[0].email, 'iat': signInTime, 'exp': expired};
-							var token = modtoken.createToken(data, res);
-							res.json({"status":true, "message":"Login success", "token": token});
-						}
-					}else{
-						res.json({"status":false, "message":"Error retrieving data"});
-					}
-				});
-			});
-		}
-	};
-}
-
-module.exports = new User();
->>>>>>> 06bf3b7e7983433833970950b103550e962150d2
--- api/views/3.d7f3c37aa1622f078fe9.chunk.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([3],{400:function(a,e){a.exports=[{res:"data"}]}});
