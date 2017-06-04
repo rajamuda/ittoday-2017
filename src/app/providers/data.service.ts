6150963c832da311265f0292430329dec7555c8c
Fadhlal Khaliq Surado <fksutan.rajomudo@gmail.com>
2017-06-04 21:53:41 +0700

    Funsionalitas submisi loma

--- src/app/providers/data.service.ts
+++ src/app/providers/data.service.ts
@@ -10,22 +10,34 @@ export class DataService{
 	public isLoggedIn = new Subject<boolean>();
 
 	public baseTitle = 'IT Today IPB 2017 - CreativITy';
-	public baseUrl = 'http://localhost:4200';
+	public baseUrl = 'https://localhost:4201';
+
+	/* User */
 	public urlLogin = this.baseUrl+'/api/user/login';
 	public urlRegister = this.baseUrl+'/api/user/register';
 	public urlSession = this.baseUrl+'/api/user/session';
 	public urlEditProfile = this.baseUrl+'/api/user/editprofile';
 	public urlShowProfile = this.baseUrl+'/api/user/showprofile';
 	public urlUploadID = this.baseUrl+'/api/user/uploadid';
+
+	/* AppsToday */
 	public urlHasRegistApps = this.baseUrl+'/api/appteam/user';
 	public urlRegistApps = this.baseUrl+'/api/appteam/create';
 	public urlRegistAppsMember = this.baseUrl+'/api/appteam/registermember';
+
+	/* HackToday */
 	public urlHasRegistHack = this.baseUrl+'/api/hackteam/user';
 	public urlRegistHack = this.baseUrl+'/api/hackteam/create';
 	public urlRegistHackMember = this.baseUrl+'/api/hackteam/registermember';
+
+	/* Seminar */
 	public urlHasRegistSeminar = this.baseUrl+'/api/seminar/user';
 	public urlRegistSeminar = this.baseUrl+'/api/seminar/register';
 
+	/* custom VAR */
+	public writeUpSubmission = false;
+	public firstAppsSubmission = false;
+	public secondAppsSubmission = false;
 
 	loginAnnounced$ = this.isLoggedIn.asObservable();
 
